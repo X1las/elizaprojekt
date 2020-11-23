81354e9454ec13c4f2a4eb6a8d2ca61fe7de8558
X1las <dahepix1@gmail.com>
2020-11-23 22:31:23 +0100

    Fixed static variables
    Fiksede words array så den kan blive kaldt i eliza.java uden at lave den til et objekt først.

--- eliza/keywords.java
+++ eliza/keywords.java
@@ -1,7 +1,8 @@
 package eliza;
 
-public class keywords {
-    
-    public static String[] words = {"Hello","Hey","Hi"};
+// Keyword library
 
+public class keywords {
+    // Static string array for keywords (probably remaking this later)
+    static String[] words = {"Hello","Hey","Hi"};
 }
