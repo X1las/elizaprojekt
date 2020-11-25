9adef431e5e2072ef968b06fb732e6fefc4fa8e9
Xilas <dahepix1@gmail.com>
2020-11-25 17:16:56 +0100

    Fixed converter method

--- eliza/eliza.java
+++ eliza/eliza.java
@@ -45,15 +45,29 @@ public class eliza {
     // Method for converting a string sentence into an array of words and symbols in order
     static String[] convertInput(String input) {
         
-        // Assigning input a temp string to replace all "!" with empty space
-        String temp = input.replaceAll("!","");
+        // Converting input into a temporary character array tempa
+        char[] tempa = input.toCharArray();
         
-        temp = temp.replaceAll("?",""); // Doing the same, but with all "?"
+        // Going through all characters of the array to find ! and ? and replacing them by space
+        for (int i = 0 ; i < tempa.length ; i++)
+        {
+            if (tempa[i] == '!')
+            {
+                tempa[i] = ' ';
+            }
+            if (tempa[i] == '?')
+            {
+                tempa[i] = ' ';
+            }
+        }
+
+        // Converting character array to string
+        String temp = new String(tempa);
         
-        // Using String.split() to divide modified text input into an array of words
+        // Splitting the string at each space in the sentence. If i don't add true as the second parameter, it will exclude empty spaces between doubled spaced words as array entries
         String[] Output = temp.split(" ");
-        
-        return Output; // Returns the array
+
+        return Output; // Returns an array of whole words.
     }
 
     // Empty method for analyzing an array of words and compare them to keywords in order to find a response
